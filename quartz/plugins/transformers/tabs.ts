fbc212d79eeab0a3bfadd270d781c837dd999c32
unknown <it-tanglizhen@yihongdc.com>
2026-06-11 19:15:44 +0800

    fix: use textTransform hook instead of remarkPlugins for Tabs plugin

--- quartz/plugins/transformers/tabs.ts
+++ quartz/plugins/transformers/tabs.ts
@@ -1,17 +1,10 @@
 ﻿import { QuartzTransformerPlugin } from "../types"
-import { visit } from "unist-util-visit"
 
 export const Tabs: QuartzTransformerPlugin = () => {
   return {
     name: "Tabs",
-    remarkPlugins: [
-      () => (tree) => {
-        visit(tree, "text", (node: { value: string }) => {
-          if (node.value.includes("\t")) {
-            node.value = node.value.replace(/\t/g, "\u00A0\u00A0\u00A0\u00A0")
-          }
-        })
-      },
-    ],
+    textTransform: (_ctx, src: string) => {
+      return src.replace(/\t/g, "\u00A0\u00A0\u00A0\u00A0")
+    },
   }
 }
