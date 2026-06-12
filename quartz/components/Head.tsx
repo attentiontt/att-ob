37519c7525cd48801b295c144d444dd44d3d566c
unknown <it-tanglizhen@yihongdc.com>
2026-06-12 10:39:09 +0800

    fix: remove pre-wrap CSS to avoid extra blank lines

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,7 +40,6 @@ export default (() => {
       <head>
         <title>{title}</title>
                         <meta charSet="utf-8" />
-        <style>{`.popover-hint { white-space: pre-wrap; tab-size: 4; }`}</style>
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
